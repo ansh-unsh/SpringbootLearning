f81e25f054fad80797538bae0bbbc821f5449e78
ansh <anshbhardwaj0202@gmail.com>
2025-06-27 07:33:05 -0400

    commit

---
+++ src/main/java/in28min/loosecoupling/AppGamingBasic.java
@@ -0,0 +1,20 @@
+package in28min.loosecoupling;
+
+public class AppGamingBasic {
+    public static void main(String[] args) {
+
+//        var marioGame = new MarioGame();
+//        var gameRunner = new GameRunner(marioGame);
+
+        var superContraGame = new SuperContraGame();
+        var gameRunner = new GameRunner(superContraGame);
+
+
+
+        gameRunner.run();
+
+
+
+
+    }
+}
---
+++ src/main/java/in28min/loosecoupling/GameRunner.java
@@ -0,0 +1,23 @@
+package in28min.loosecoupling;
+
+public class GameRunner {
+    //private MarioGame game;
+    private SuperContraGame game;
+
+//    public GameRunner(MarioGame game) {
+//        this.game = game;
+//
+//    }
+
+    public GameRunner(SuperContraGame game) {
+        this.game = game;
+    }
+
+    public void run() {
+        System.out.println("Running Game: "+game);
+        game.up();
+        game.down();
+        game.left();
+        game.right();
+    }
+}
